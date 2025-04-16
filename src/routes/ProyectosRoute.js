81ad02fb99dd4b5e320510915ed23060537604b5
Valentina210 <valentinaub20@gmail.com>
2025-04-15 23:31:35 -0500

    bac-subirProyecto

--- src/routes/ProyectosRoute.js
+++ src/routes/ProyectosRoute.js
@@ -1,9 +1,11 @@
 const {Router} = require("express");
 const {createProyectos,GetProyectosAll,GetProyectosId,PutProyectosId,DeleteProyectosId}=require("../controllers/ProyectosControllers")
+const {ValidacionSchema} = require('../middlewares/validacionMiddlewares')
+const {SubirProyectosSchema} = require("../schemas/ProyectosSchema.js")
 
 const router = Router();
 
-router.route("/").get(GetProyectosAll).post(createProyectos);
+router.route("/").get(GetProyectosAll).post(ValidacionSchema(SubirProyectosSchema), createProyectos);;
 router.route("/:id").get(GetProyectosId).put(PutProyectosId).delete(DeleteProyectosId);
 
 module.exports =  router;
