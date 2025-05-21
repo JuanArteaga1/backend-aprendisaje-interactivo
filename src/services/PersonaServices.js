6616c6c9b51daf444d54d20b509b89792c20d603
tostako <juan.arteaga.f@uniautonoma.edu.co>
2025-05-21 11:00:43 -0500

    solo falta lo de simulaciones pero hay un error

--- src/services/PersonaServices.js
+++ src/services/PersonaServices.js
@@ -2,7 +2,7 @@ const PersonaModel = require('../models/Personas');
 
 
 
-exports.createPersona = async (Persona) =>{
+exports.createPersona = async (Persona) => {
     const newPersona = new PersonaModel(Persona)
     await newPersona.save()
     return newPersona
@@ -15,11 +15,16 @@ exports.getPersonaId = async (id) => {
     const Persona = await PersonaModel.findById(id);
     return Persona;
 };
-exports.PutPersonaId = async (id,Persona) => {
-    const Personas = await PersonaModel.findByIdAndUpdate(id,Persona);
+exports.PutPersonaId = async (id, Persona) => {
+    const Personas = await PersonaModel.findByIdAndUpdate(id, Persona);
     return Personas;
 };
-exports.DeletePersonaId = async (id,Persona) => {
-    const Personas = await PersonaModel.findByIdAndDelete(id,Persona);
-    return Personas;
+exports.DeletePersonaId = async (id) => {
+    try {
+        const personaEliminada = await PersonaModel.findOneAndDelete({ UsuarioId: id});
+        return personaEliminada;
+    } catch (error) {
+        console.error('Error eliminando persona por UsuarioId:', error);
+        throw error;
+    }
 };
